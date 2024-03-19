728f427afcec03e2f11b16758915cda951032a92
Maria Gabriella Victor <109552474+gabriellavsx@users.noreply.github.com>
2024-03-19 10:58:39 -0300

    modulo

--- Atividades de Qualidade de Software/Atividade 1/index.js
+++ Atividades de Qualidade de Software/Atividade 1/index.js
@@ -1,9 +1,9 @@
-const Aluno = require("./aluno");
-const Turma = require("./turma");
-const Turma_presencial = require("./turma_presencial");
+const Aluno = require('./aluno');
+const Turma = require('./turma');
+const Turma_presencial = require('./turma_presencial');
 
-const aluno = new Aluno('Maria Gabriella','mariagabriellavictor','2143364');
+const Aluno = new Aluno('Maria Gabriella','mariagabriellavictor','2143364');
 const Turma = new Turma("ES46A",8);
 const Turma_presencial = new Turma_presencial(75, 8, "ES46A");
 
-console.log('Aluno: ${aluno.nome}, RA: ${aluno.ra}\n')
+console.log(`Aluno: ${aluno.nome}\nRA: ${aluno.ra}\n`)
